361d5b56ade983321744dfbced0e48eb26d701f4
adrian <alodeiroperezrgmail.com>
2018-11-10 16:03:50 +0100

    Numeros Naturales

---
+++ Ejercicios Anexo A/src/Suma_N_Naturales.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Suma_N_Naturales {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
+		
+		int n;
+		int suma = 0;
+		
+		System.out.println("Introduce el numero de numeros naturales a sumar: ");
+		n = in.nextInt();
+		
+		if (n > 0) {
+			for (int natural = 1; natural <= n; natural++) {
+				suma = suma + natural;
+			}
+			System.out.println("La suma de " + n + " naturales es " + suma);
+		}
+		else {
+			System.out.println("El valor introducido no es válido");
+		}
+				in.close();
+	}
+}
